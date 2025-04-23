fa622cddef5a18d92fb3ec9c653a6a4ce0d9678e
홍혁준(WM) <robert@wemade.com>
2025-04-23 17:43:28 +0900

    초기 세팅

---
+++ src/main/java/org/wemade/blockchaincore/BlockchainCoreApplication.java
@@ -0,0 +1,13 @@
+package org.wemade.blockchaincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainCoreApplication.class, args);
+    }
+
+}
