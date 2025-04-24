fbdb815b1c6f9b0f119eefa66256f0a5f42a1ef6
홍혁준(WM) <robert@wemade.com>
2025-04-24 13:49:12 +0900

    초기 세팅

---
+++ src/main/java/com/wemade/blockchaincore/model/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.wemade.blockchaincore.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BaseResponse {
+
+    private int result;
+    private String resultString;
+
+}
